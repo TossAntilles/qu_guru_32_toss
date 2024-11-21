54294b668966c50c5a29cba967b0612ab80c9b6e
Anton Bogutskii <schicksalkreuzung@gmail.com>
2024-11-21 03:49:43 +0300

    Fixed class names

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -7,7 +7,7 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
     @Test
-    void successfulSearchTest() {
+    void successfulSearchTestGoogle_textGoogle() {
         Configuration.pageLoadStrategy = "eager";
         open("https://www.google.ru/");
         $("[name=q]").setValue("selenide").pressEnter();
@@ -15,14 +15,14 @@ public class SearchTests {
     }
 
     @Test
-    void successfulYaSearchTest() {
+    void successfulYaSearchTestGoogle_textGoogle() {
         open("https://ya.ru/");
         $("[name=text]").setValue("selenide").pressEnter();
         $("[id=search-result]").shouldHave(text("ru.selenide.org"));
     }
 
     @Test
-    void successfulYaSearchTest() {
+    void successfulYaSearchTest_textVK() {
         open("ya.ru");
         $("[name=text]").setValue("VK").pressEnter();
         $("[id=search-result]").shouldHave(text("vk.ru"));
